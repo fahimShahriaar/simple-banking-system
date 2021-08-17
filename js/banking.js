aedf27ff3b5f0aed6f1c9b0ff2bad33b02ed7b22
fem97 <fem.nwu@gmail.com>
2021-08-17 23:13:16 +0600

    updated

--- js/banking.js
+++ js/banking.js
@@ -1,25 +1,51 @@
-document.getElementById('deposit-btn').addEventListener('click', function () {
-    const depositInput = Number(document.getElementById('deposit-input').value);
-    let totalDeposit = Number(document.getElementById('total-deposit').innerText);
-    totalDeposit += depositInput;
-    document.getElementById('total-deposit').innerText = totalDeposit;
+function getInputValue(inputId) {
+    const inputField = Number(document.getElementById(inputId).value);
     // reset input
-    document.getElementById('deposit-input').value = '';
-    // updating total balance
+    document.getElementById(inputId).value = '';
+    return inputField;
+}
+
+function updateTotalField(totalFieldId, inputValue) {
+    let totalFieldAmount = Number(document.getElementById(totalFieldId).innerText);
+    totalFieldAmount += inputValue;
+    document.getElementById(totalFieldId).innerText = totalFieldAmount;
+}
+
+function updateTotalBalance(field, fieldInput) {
     let totalBalance = Number(document.getElementById('total-balance').innerText);
-    totalBalance += depositInput;
+    if (field === 'deposit') {
+        totalBalance += fieldInput;
+    }
+    else if (field === 'withdraw' && totalBalance > 0) {
+        totalBalance -= fieldInput;
+    }
     document.getElementById('total-balance').innerText = totalBalance;
+}
+
+
+document.getElementById('deposit-btn').addEventListener('click', function () {
+    // get deposit input
+    const depositInput = getInputValue('deposit-input');
+    if (depositInput > 0) {
+        // update total deposit
+        updateTotalField('total-deposit', depositInput);
+        // update total balance
+        updateTotalBalance('deposit', depositInput);
+    }
+
 })
 
 document.getElementById('withdraw-btn').addEventListener('click', function () {
-    const withdrawInput = Number(document.getElementById('withdraw-input').value);
-    let totalWithdraw = Number(document.getElementById('total-withdraw').innerText);
-    totalWithdraw += withdrawInput;
-    document.getElementById('total-withdraw').innerText = totalWithdraw;
-    // reset input
-    document.getElementById('withdraw-input').value = '';
-    // updating total balance 
-    let totalBalance = Number(document.getElementById('total-balance').innerText);
-    totalBalance -= withdrawInput;
-    document.getElementById('total-balance').innerText = totalBalance;
+    // get withdraw input
+    const withdrawInput = getInputValue('withdraw-input');
+    if (withdrawInput > 0) {
+        const totalBalance = Number(document.getElementById('total-balance').innerText);
+        if (totalBalance > 0) {
+            // update total withdraw
+            updateTotalField('total-withdraw', withdrawInput);
+        }
+
+        // update total balance
+        updateTotalBalance('withdraw', withdrawInput);
+    }
 })
